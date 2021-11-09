06d76db4cc7122e42ac4796a802d5e1c515f64e5
Ngo Ngoc Tri <vfa.trinn@gmail.com>
2021-11-09 09:54:39 +0700

    first commit

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1263750.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 59262882, 59384526, 59395570, 59737683 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/59261717/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/59261718/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/59261716/1/ammo.js', 'preload' : true},
+];
